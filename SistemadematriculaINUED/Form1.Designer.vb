f2ed3122a0004bf923fedc4d0349a1c1f74fc2b4
Lab04 <Lab04@DESKTOP-UTK7OLI>
2024-05-15 12:11:38 -0600

    Agregar archivos de proyecto.

---
+++ SistemadematriculaINUED/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnConectar = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnConectar
+        '
+        Me.btnConectar.BackColor = System.Drawing.Color.LightYellow
+        Me.btnConectar.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConectar.Image = CType(resources.GetObject("btnConectar.Image"), System.Drawing.Image)
+        Me.btnConectar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnConectar.Location = New System.Drawing.Point(216, 28)
+        Me.btnConectar.Name = "btnConectar"
+        Me.btnConectar.Size = New System.Drawing.Size(356, 80)
+        Me.btnConectar.TabIndex = 0
+        Me.btnConectar.Text = "REALIZAR CONEXION"
+        Me.btnConectar.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 157)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(755, 326)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SteelBlue
+        Me.ClientSize = New System.Drawing.Size(779, 495)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.btnConectar)
+        Me.Name = "Form1"
+        Me.Text = "SISTEMA DE MATRICULA INUED - Maestros"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnConectar As Button
+    Friend WithEvents DataGridView1 As DataGridView
+End Class
---
+++ SistemadematriculaINUED/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.OleDb
+
+Public Class Form1
+
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+        Dim con As New OleDbConnection("Provider=Microsoft.Jet.oledb.4.0; Data Source=F:\PROGRAMAS\PROGRAMACION\SISTEMA MATRICULA INUED\SistemadematriculaINUED\Base de datos\MatriculaINUED(2002-2003).mdb")
+        Try
+            con.Open()
+            MsgBox("Conectado")
+            Dim query = "Select * from MAESTROS"
+            Dim adap As New OleDbDataAdapter(query, con)
+            Dim dt As New DataTable
+            adap.Fill(dt)
+            DataGridView1.DataSource = dt
+        Catch ex As Exception
+            MsgBox("No se conecto por: " & ex.Message)
+        End Try
+    End Sub
+End Class
